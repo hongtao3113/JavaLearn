12a58ac110a59dc646908573d7e9427317016332
中金-开发-王凯 <wangkai@zhongjincloud.com>
2020-02-14 00:52:49 +0800

    IntegerCache缓存的最大最小值

--- src/test/java/JavaLearn/Test.java
+++ src/test/java/JavaLearn/Test.java
@@ -11,5 +11,16 @@ public class Test {
         String s1 = "通话",s2="重地";
         System.out.println(String.format("s1:%d s2:%d",s1.hashCode(),s2.hashCode()));
         Object o = new Object();
+        Integer i1=127,i2=127;
+        Integer i3=128,i4=128;
+        Integer i5=-128,i6=-128;
+        System.out.println(i1==i2
+        );
+        System.out.println(i3==i4
+        );
+        System.out.println(i5==i6
+        );
+        System.out.println("你哈".charAt(0));
+
     }
 }
